43d835f0d5c9f5a8063ca8ed1c674195c5aedd4a
Katie Blizzard <kblizzard11@gmail.com>
2018-12-18 00:57:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name)
+  return drivers
+}
+
+destructivelyAppendDriver("Ralph");
+
+
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name)
+  return drivers
+}
+
+destructivelyPrependDriver("A")
+
+
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+  return drivers
+}
+
+destructivelyRemoveLastDriver()
+
+
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift()
+  return drivers
+}
+
+destructivelyRemoveFirstDriver("C")
+
+
+
+function appendDriver(name) {
+  const drivers2 = [...drivers, name]
+  return drivers2
+}
+
+appendDriver("Salem")
+
+
+
+prependDriver()
+
+removeLastDriver()
+
+removeFirstDriver()
+
+
